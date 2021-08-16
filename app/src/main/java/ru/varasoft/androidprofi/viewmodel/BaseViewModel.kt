6588806e16ab45382a901f3b2b695a2a28d0647e
Вадим <vraspopin@inbox.ru>
2021-08-16 20:56:18 +0300

    Корутины

--- app/src/main/java/ru/varasoft/androidprofi/viewmodel/BaseViewModel.kt
+++ app/src/main/java/ru/varasoft/androidprofi/viewmodel/BaseViewModel.kt
@@ -2,19 +2,31 @@ package ru.varasoft.androidprofi.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.*
 import ru.varasoft.androidprofi.model.data.AppState
-import ru.varasoft.androidprofi.rx.SchedulerProvider
 
 abstract class BaseViewModel<T : AppState>(
-    protected open val liveDataForViewToObserve: MutableLiveData<T> = MutableLiveData(),
-    protected open val compositeDisposable: CompositeDisposable = CompositeDisposable(),
-    protected open val schedulerProvider: SchedulerProvider = SchedulerProvider()
+    protected open val liveDataForViewToObserve: MutableLiveData<T> = MutableLiveData()
+
 ) : ViewModel() {
+
+    protected val viewModelCoroutineScope = CoroutineScope(
+        Dispatchers.Main
+                + SupervisorJob()
+                + CoroutineExceptionHandler { _, throwable ->
+            handleError(throwable)
+        })
 
     abstract fun getData(word: String, isOnline: Boolean)
 
     override fun onCleared() {
-        compositeDisposable.clear()
+        super.onCleared()
+        cancelJob()
     }
+
+    protected fun cancelJob() {
+        viewModelCoroutineScope.coroutineContext.cancelChildren()
+    }
+
+    abstract fun handleError(error: Throwable)
 }
--- app/src/main/java/ru/varasoft/androidprofi/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/varasoft/androidprofi/viewmodel/MainViewModel.kt
@@ -1,8 +1,11 @@
 package ru.varasoft.androidprofi.viewmodel
 
 import androidx.lifecycle.LiveData
-import io.reactivex.observers.DisposableObserver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.varasoft.androidprofi.model.data.AppState
+import ru.varasoft.androidprofi.utils.parseSearchResults
 import ru.varasoft.androidprofi.view.main.MainInteractor
 
 class MainViewModel(
@@ -14,28 +17,17 @@ class MainViewModel(
     }
 
     override fun getData(word: String, isOnline: Boolean) {
-        compositeDisposable.add(
-            interactor.getData(word, isOnline)
-                .subscribeOn(schedulerProvider.io())
-                .observeOn(schedulerProvider.ui())
-                .doOnSubscribe { liveDataForViewToObserve.value = AppState.Loading(null) }
-                .subscribeWith(getObserver())
-        )
+        liveDataForViewToObserve.value = AppState.Loading(null)
+        cancelJob()
+        viewModelCoroutineScope.launch { startInteractor(word, isOnline) }
     }
 
-    private fun getObserver(): DisposableObserver<AppState> {
-        return object : DisposableObserver<AppState>() {
-
-            override fun onNext(appState: AppState) {
-                liveDataForViewToObserve.value = appState
-            }
-
-            override fun onError(e: Throwable) {
-                liveDataForViewToObserve.value = AppState.Error(e)
-            }
-
-            override fun onComplete() {
-            }
-        }
+    private suspend fun startInteractor(word: String, isOnline: Boolean) = withContext(Dispatchers.IO) {
+        liveDataForViewToObserve.postValue(parseSearchResults(interactor.getData(word, isOnline)))
     }
+
+    override fun handleError(error: Throwable) {
+        liveDataForViewToObserve.postValue(AppState.Error(error))
+    }
+
 }
